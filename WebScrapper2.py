2bfebb051351999cc0f2d4098ef0ced3ed208210
udogra <yaderixofficial@gmail.com>
2024-07-15 13:20:44 -0600

    Initial commit

---
+++ WebScrapper2.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+ 
+webpage = requests.get(f'https://www.scrapethissite.com/pages/forms/').text
+soup = BeautifulSoup(webpage,'lxml')
+players = soup.find_all('tr')[1:]
+#print(players)
+team_name = []
+year = []
+win=[]
+losses=[]
+for i in players:
+    name = i.find_all('td')[0].text.strip()
+    yr = i.find_all('td')[1].text.strip()
+    wn = i.find_all('td')[2].text.strip()
+    ls = i.find_all('td')[3].text.strip()
+    team_name.append(name)
+    year.append(yr)
+    win.append(wn)
+    losses.append(ls)
+ 
+data = pd.DataFrame({'Team Name': team_name,
+                     "Year": year,
+                    'Wins':win,
+                     'Losses': losses })
+ 
+print(data)
+st.table(data)
